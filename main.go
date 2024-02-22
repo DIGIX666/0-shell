25bfcecea0c248c8b6318b1002a7269eff95e278
théo dub <theodub@MacThox.localdomain>
2024-02-22 14:06:13 +0100

    finish scripting -> go to work in jobs

--- main.go
+++ main.go
@@ -2,69 +2,29 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"log"
 	"os"
-	"os/exec"
-	"strings"
 )
 
 func main() {
-	// Get the initial working directory.
-	initialDir, err := os.Getwd()
+	initialDir, err := getInitialDir()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	scanner := bufio.NewScanner(os.Stdin)
-
 	for {
-		// Display current directory.
-		currentDir, err := os.Getwd()
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("\033[32m%s\033[0m $ ", currentDir)
-
-		// Read command.
-		scanner.Scan()
-		line := scanner.Text()
-		args := strings.Fields(line)
-
-		// Handle exit command.
-		if len(args) > 0 && args[0] == "exit" {
+		displayPrompt()
+		if !scanner.Scan() {
 			break
 		}
-
-		// Handle cd command.
-		if len(args) > 0 && args[0] == "cd" {
-			newDir := initialDir
-			if len(args) > 1 {
-				newDir = args[1]
-			}
-
-			err := os.Chdir(newDir)
-			if err != nil {
-				log.Printf("cd: %s\n", err)
-			}
-
-			continue
+		line := scanner.Text()
+		if line == "exit" {
+			break
 		}
-
-		// Run command and wait for it to finish.
-		cmd := exec.Command(args[0], args[1:]...)
-		cmd.Stdin = os.Stdin
-		cmd.Stderr = os.Stderr
-		cmd.Stdout = os.Stdout
-
-		err = cmd.Run()
-
-		if err != nil {
-			log.Printf("failed to execute process: %s\n", err)
-		}
+		processLine(line, initialDir)
 	}
-
-	if scanner.Err() != nil {
+	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 }
